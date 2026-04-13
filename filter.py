92f3c886451c16aa96a192169356bb06df837848
maxell666 <maxell.one@online.fr>
2026-04-13 19:08:09 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -3,11 +3,24 @@ KEYWORDS = [
     "sanctions", "conflict", "missile",
     "inflation", "interest rate", "fed", "ecb"
 ]
-
+EXCLUDE_KEYWORDS = [
+    "executions",
+    "killed",
+    "murder",
+    "death",
+    "crime",
+    "stabbing"
+]
 
 def is_relevant(text):
     text = text.lower()
 
+    # ❌ blacklist (prioritaire)
+    for k in EXCLUDE_KEYWORDS:
+        if k in text:
+            return False
+
+    # ✅ whitelist
     for k in KEYWORDS:
         if k in text:
             return True
